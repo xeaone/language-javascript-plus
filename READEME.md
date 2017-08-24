8625de8b9117812b64bdf8baf1a94e23a72b8ca7
Alexander Elias <alex.steven.elias@gmail.com>
2017-08-24 14:36:56 -0700

    base

---
+++ READEME.md
@@ -0,0 +1,11 @@
+
+# JavaScript Plus
+Syntax highlighting for template strings in Atom.
+
+##Triggers
+- `template: `
+- `html: `
+- `html = `
+- `template = `
+- `style: `
+- `style = `
---
+++ test/test.js
@@ -0,0 +1,20 @@
+
+var obj = {
+	template: `
+		<div>hello world</div>
+	`,
+	la: ''
+}
+
+var template = `
+	${var}
+	<div>hello world</div>
+`
+var la.innerHTML = `
+	<div>la</div>
+`;
+var style = `
+	.class {
+		background-color: url('asdfas');
+	}
+`
